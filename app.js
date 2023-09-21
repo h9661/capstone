82c94c465d23d514ee4f77d5b9c4dfa56258fca5
h9661 <guscksdn123@gmail.com>
2023-09-21 21:00:29 +0900

    db 연결, passport 추가, session 설정, 인덱스 라우터 설정, 에러 핸들링 설정

--- app.js
+++ app.js
@@ -5,33 +5,68 @@ const path = require("path");
 const session = require("express-session");
 const dotenv = require("dotenv");
 
-dotenv.config();
+const indexRouter = require("./backend/routes/index");
+const { sequelize } = require("./backend/models/index");
+const passportConfig = require("./backend/passport/index");
+const passport = require("./backend/passport/index");
 
+sequelize
+  .sync({ force: false }) // force: true -> 서버 실행 시마다 테이블 재생성
+  .then(() => {
+    console.log("데이터베이스 연결 성공");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+passportConfig();
+
+dotenv.config();
 const app = express();
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "html");
-
-app.use(morgan("dev"));
 
 // 각각의 static 파일들 경로 설정
 app.use("/images", express.static(path.join(__dirname, "backend/images")));
 app.use("/videos", express.static(path.join(__dirname, "backend/videos")));
 app.use("/statics", express.static(path.join(__dirname, "frontend/public")));
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-// 세션 설정
-const sessionOption = {
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-};
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  // 에러 처리 미들웨어
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  console.error(err);
+  res.send({
+    message: err.message,
+    error: err,
+  });
+});
 
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기 중");
