914969acb74381e723d8f49daaf6fa40cfa8bb49
h9661 <guscksdn123@gmail.com>
2023-10-29 20:27:55 +0900

    [Feat] 라우터들 추가 구현
    post, comment, recomment를 처리하는 엔드포인트를 구현했습니다.
    
    user와 관련된 것들을 처리하는 라우터를 구현하였습니다.

---
+++ backend/routes/postRouter.js
@@ -0,0 +1,148 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/post");
+const Comment = require("../models/comment");
+const Recomment = require("../models/recomment");
+const User = require("../models/user");
+const { isLoggedIn } = require("../middlewares");
+
+// 카태고리에 해당하는 글들을 얻어옵니다.
+router.get("/:postCategory", isLoggedIn, async (req, res) => {
+  let postCategory = req.params.postCategory;
+
+  let categorizedPost = await Post.findAll({
+    where: { category: postCategory },
+    order: [["createdAt", "DESC"]],
+  });
+
+  res.json(categorizedPost);
+});
+
+// 글의 상세 정보를 얻어옵니다.
+router.get("/:postId/detail", isLoggedIn, async (req, res) => {
+  let postId = req.params.postId;
+
+  let postDetail = await Post.findOne({ where: { id: postId } });
+
+  res.json(postDetail);
+});
+
+// 글을 작성합니다.
+router.post("/write", isLoggedIn, async (req, res) => {
+  let { title, category, content, password, writer } = req.body;
+
+  try {
+    await Post.create({
+      title: title,
+      category: category,
+      content: content,
+      password: password,
+      writer: writer,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
+  res.json({ result: "success" });
+});
+
+// 글을 수정합니다.
+router.put("/:postId/update", isLoggedIn, async (req, res) => {
+  let postId = req.params.postId;
+  let { title, category, content, password } = req.body;
+
+  try {
+    await Post.update(
+      {
+        title: title,
+        category: category,
+        content: content,
+        password: password,
+      },
+      { where: { id: postId } }
+    );
+  } catch (err) {
+    console.error(err);
+  }
+
+  res.json({ result: "success" });
+});
+
+// 글을 삭제합니다.
+router.delete("/:postId/delete", isLoggedIn, async (req, res) => {
+  let postId = req.params.postId;
+
+  try {
+    await Post.destroy({ where: { id: postId } });
+  } catch (err) {
+    console.error(err);
+  }
+
+  res.json({ result: "success" });
+});
+
+// 댓글을 작성합니다.
+router.post("/:postId/comment/write", isLoggedIn, async (req, res) => {
+  let postId = req.params.postId;
+  let { writer, content, password } = req.body;
+
+  try {
+    await Comment.create({
+      writer: writer,
+      content: content,
+      post_id: postId,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
+  res.json({ result: "success" });
+});
+
+// 댓글을 삭제합니다.
+router.delete("/:postId/comment/:commentId/delete", isLoggedIn, async (req, res) => {
+  let commentId = req.params.commentId;
+
+  try {
+    await Comment.destroy({ where: { id: commentId } });
+  } catch (err) {
+    console.error(err);
+  }
+
+  res.json({ result: "success" });
+});
+
+// 대댓글을 작성합니다.
+router.post("/:postId/comment/:commentId/recomment/write", isLoggedIn, async (req, res) => {
+  let postId = req.params.postId;
+  let commentId = req.params.commentId;
+  let { writer, content, password } = req.body;
+
+  try {
+    await Recomment.create({
+      writer: writer,
+      content: content,
+      post_id: postId,
+      comment_id: commentId,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
+  res.json({ result: "success" });
+});
+
+// 대댓글을 삭제합니다.
+router.delete("/:postId/comment/:commentId/recomment/:recommentId/delete", isLoggedIn, async (req, res) => {
+  let recommentId = req.params.recommentId;
+
+  try {
+    await Recomment.destroy({ where: { id: recommentId } });
+  } catch (err) {
+    console.error(err);
+  }
+
+  res.json({ result: "success" });
+});
+
+module.exports = router;
---
+++ backend/routes/userRouter.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/post");
+const User = require("../models/user");
+const Comment = require("../models/comment");
+const Recomment = require("../models/recomment");
+
+// 유저의 정보를 얻어옵니다.
+router.get("/:username/info", async (req, res) => {
+  let username = req.params.username;
+
+  let userInfo = await User.findOne({
+    where: { username: username },
+    attributes: ["username", "email", "createdAt"],
+  });
+
+  res.json(userInfo);
+});
+
+// 유저가 쓴 글을 얻어옵니다.
+router.get("/:username/posts", async (req, res) => {
+  let username = req.params.username;
+
+  let userPosts = await Post.findAll({
+    where: { writer: username },
+    order: [["createdAt", "DESC"]],
+  });
+
+  res.json(userPosts);
+});
+
+// 유저가 쓴 댓글 및 대댓글을 얻어옵니다.
+router.get("/:username/comments", async (req, res) => {
+  let username = req.params.username;
+
+  let userComments = await Comment.findAll({
+    where: { writer: username },
+    order: [["createdAt", "DESC"]],
+  });
+
+  let userRecomments = await Recomment.findAll({
+    where: { writer: username },
+    order: [["createdAt", "DESC"]],
+  });
+
+  let allWritedComments = userComments.concat(userRecomments);
+
+  res.json(allWritedComments);
+});
+
+// 유저의 계정을 삭제합니다.
+router.delete("/:username/delete", async (req, res) => {
+  let username = req.params.username;
+
+  await User.destroy({ where: { username: username } });
+
+  res.json({ result: "success" });
+});
+
+// 유저의 정보를 수정합니다.
+router.put("/:username/update", async (req, res) => {
+  let username = req.params.username;
+  let { password, email } = req.body;
+
+  await User.update(
+    {
+      password: password,
+      email: email,
+    },
+    { where: { username: username } }
+  );
+
+  res.json({ result: "success" });
+});
+
+module.exports = router;
