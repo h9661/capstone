4520932156072d7cdcf75dcee7e06b0f427977a9
h9661 <guscksdn123@gmail.com>
2023-11-13 22:09:04 +0900

    Update middleware to send error status instead of redirect

--- backend/middlewares/index.js
+++ backend/middlewares/index.js
@@ -2,7 +2,7 @@ exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   } else {
-    res.redirect("/login");
+    res.status(401).send("로그인이 필요합니다.");
   }
 };
 
@@ -10,6 +10,6 @@ exports.isNotLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     return next();
   } else {
-    res.redirect("/");
+    res.status(401).send("로그인한 사용자는 접근할 수 없습니다.");
   }
 };
